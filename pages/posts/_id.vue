f2c69c2e66a742de8e683416804965130c037712
Lucas <lucas.lubasinski11@gmail.com>
2021-06-07 12:29:18 +0200

    finish :tada:

--- pages/posts/_id.vue
+++ pages/posts/_id.vue
@@ -1,9 +1,32 @@
 <template>
-  <PostComponent/>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Post#{{post.id}}
+      </h1>
+      <div>
+        <h1>{{ post.title}}</h1>
+        <p>{{ post.body}}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
- 
+  head() {
+    return {
+      title: this.post.title
+    }
+  },
+  data() {
+    return {
+      post: {}
+    }
+  },
+  async asyncData({ params, $http }) {
+    const post = await $http.$get(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+    return { post }
+  }
 }
 </script>
